579b488f7af4768e35dab79ed037cae4052fb792
lincolnh0 <me@lincolnh0.com>
2025-01-15 07:38:43 +0000

    Add dark theme adjustment

--- components/floating-menu.tsx
+++ components/floating-menu.tsx
@@ -22,24 +22,24 @@ export default function FloatingMenu() {
 
 
   return (
-      <div>
+      <div className={"lg:hidden"}>
         <div
-            className={`fixed my-4 origin-bottom right-16 bottom-32 flex transition flex-col gap-4 text-xs ${
-                isMobileMenuOpen ? "scale-100" : "scale-0"
+            className={`fixed my-4 origin-bottom right-5 md:right-10 bottom-14 md:bottom-20 flex transition flex-col gap-2 md:gap-4 text-xs ${
+                isMobileMenuOpen ? "scale-50 md:scale-100" : "scale-0"
             }`}>
 
           {menuItems.map((item, index) => (
               <Link key={index} href={item.href} title={item.name} onClick={toggleMobileMenu}
-                    className={"bg-gray-100 border p-4 rounded-full shadow hover:bg-gray-50"}>
+                    className={"bg-gray-100 dark:bg-gray-800 border p-4 rounded-full shadow hover:bg-gray-50 dark:hover:bg-gray-500"}>
                 <div className={"flex items-center gap-4 font-medium font-mono uppercase justify-end"}>
                   <item.icon/>
                 </div>
               </Link>
           ))}
         </div>
-        <div className="fixed bottom-16 right-16 lg:hidden">
+        <div className="fixed bottom-5 right-5 md:right-10 scale-50 md:scale-100">
           <Button
-              className={"shadow-lg rounded-full py-7 bg-gradient-to-br from-black to-gray-500 border-gray-500 border hover:scale-110 duration-300hover:from-gray-800 hover:to-gray-400 transition ease-in-out"}
+              className={"shadow-lg rounded-full py-7 bg-gradient-to-br  dark:from-gray-500 dark:to-gray-50 from-black to-gray-500 border-gray-500 border hover:scale-110 duration-300hover:from-gray-800 hover:to-gray-400 transition ease-in-out"}
               onClick={toggleMobileMenu}
           >
             <Menu/>
