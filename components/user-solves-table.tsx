2d6a621742df154ce9659ac8b70bffb3b813c90a
lincolnh0 <me@lincolnh0.com>
2025-01-15 01:06:11 +0000

    Final padding adjustments

--- components/user-solves-table.tsx
+++ components/user-solves-table.tsx
@@ -8,31 +8,31 @@ export default function UserSolvesTable({solves = []}: { solves: any[] | null })
         <table className="hidden sm:block table-fixed text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="lg:px-6 py-3">Date</th>
-            <th scope="col" className="lg:px-6 py-3">Time</th>
-            <th scope="col" className="lg:px-6 py-3">Scramble</th>
-            <th scope="col" className="lg:px-6 py-3">Video</th>
-            <th scope="col" className="lg:px-6 py-3">Verified</th>
+            <th scope="col" className="sm:px-2 md-px-4lg:px-6 py-3">Date</th>
+            <th scope="col" className="sm:px-2 md-px-4lg:px-6 py-3">Time</th>
+            <th scope="col" className="sm:px-2 md-px-4lg:px-6 py-3">Scramble</th>
+            <th scope="col" className="sm:px-2 md-px-4lg:px-6 py-3">Video</th>
+            <th scope="col" className="sm:px-2 md-px-4lg:px-6 py-3">Verified</th>
           </tr>
           </thead>
           <tbody>
           {solves?.map((solve) => (
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={solve.id}>
                 <td scope="row"
-                    className="lg:px-6 py-4">{solve.created_at.split("T")[0]}</td>
-                <td className="lg:px-6 py-4">{solve.solve_time}s</td>
-                <td className="lg:px-6 py-4">
+                    className="sm:px-2 md-px-4lg:px-6 py-4">{solve.created_at.split("T")[0]}</td>
+                <td className="sm:px-2 md-px-4lg:px-6 py-4">{solve.solve_time}s</td>
+                <td className="sm:px-2 md-px-4lg:px-6 py-4">
                   {solve.scramble}
                 </td>
-                <td className="lg:px-6 py-4">
+                <td className="sm:px-2 md-px-4lg:px-6 py-4">
                   {solve.video_url ? (
                       <Link href={solve.video_url} target="_blank" rel="noreferrer">
                         Watch
                       </Link>
                   ) : null}
                 </td>
-                {solve.verified ? (<td className="lg:px-6 py-4 text-blue-500"><BadgeCheck/></td>) : (
-                    <td className="lg:px-6 py-4"><X/></td>)}
+                {solve.verified ? (<td className="sm:px-2 md-px-4lg:px-6 py-4 text-blue-500"><BadgeCheck/></td>) : (
+                    <td className="sm:px-2 md-px-4lg:px-6 py-4"><X/></td>)}
               </tr>
           ))}
           </tbody>
